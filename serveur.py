413de5958bd4ab8e9304107b07754c9a142b11fc
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2019-03-10 11:06:21 +0100

    plusieurs canaux de chat !

--- serveur.py
+++ serveur.py
@@ -29,31 +29,47 @@ list_of_clients = {'Hub' : [], 'Blabla' : []}
 def clientthread(conn, addr): 
 
 	# sends a message to the client whose user object is connected
-	conn.send("Bienvenue dans le Hub !") 
-	conn.send("Choissisez un salon : ")
-	conn.send(s for s in list_of_clients.keys())
+	conn.send("Bienvenue dans le Hub !\n") 
+	conn.send("Choissisez un salon : \n")
+	liste_chan=''
+	for s in list_of_clients.keys() :
+		liste_chan+=s+';'
+	conn.send(liste_chan)
+	
+	chan=conn.recv(2048)[:-1]
+	while chan not in list_of_clients.keys() :
+		conn.send("Salon inexsitant\n""Choissisez un salon : \n")
+		conn.send(liste_chan)
+		chan=conn.recv(2048)[:-1]
+	
+	remove(conn, 'Hub')
+	list_of_clients[chan].append(conn)
+	conn.send("Bienvenue dans "+chan+'\n')
+	broadcast(addr[0]+" est entre dans le salon", conn, chan)
+	
+	
 	while True: 
 			try: 
 				message = conn.recv(2048) 
 				if message: 
-
 		  #prints on the terminal :  message and address of the user
 					print("<" + addr[0] + "> " + message) 
 
 					# Calls broadcast function to send message to all 
 					message_to_send = "<" + addr[0] + "> " + message 
-					broadcast(message_to_send, conn) 
+					broadcast(message_to_send, conn, chan) 
 
 				else: 
 					#remove connection when it's broken
-					remove(conn) 
-
+					remove(conn, chan) 
+					broadcast(addr[0]+" a quitte le salon", conn, chan)
+					break
 			except: 
 				continue
 
 #Broadcast the message to all clients except the one who sent the message
-def broadcast(message, connection): 
-	for clients in list_of_clients: 
+def broadcast(message, connection, chan): 
+	for clients in list_of_clients[chan]: 
 		if clients!=connection: 
 			try: 
 				clients.send(message) 
@@ -64,9 +80,9 @@ def broadcast(message, connection):
 				remove(clients) 
 
 #removes the client from the list of clients
-def remove(connection): 
-	if connection in list_of_clients: 
-		list_of_clients.remove(connection) 
+def remove(connection, chan): 
+	if connection in list_of_clients[chan]: 
+		list_of_clients[chan].remove(connection) 
 
 #Sends a message when the server is in place
 print("Your server is up.")
