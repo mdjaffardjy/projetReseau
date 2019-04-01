04758b932c333204d3e0867c643a460f7a903362
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2019-04-01 17:37:18 +0200

    tout

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
